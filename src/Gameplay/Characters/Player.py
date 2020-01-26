8d1a18bbf47154f2daedea3d053628af701838f7
Adil7777 <57364623+Adil7777@users.noreply.github.com>
2020-01-26 23:24:17 +0600

    Update Player.py
    Добавил спрайт игрока и его анимацию(удары, бег)

--- src/Gameplay/Characters/Player.py
+++ src/Gameplay/Characters/Player.py
@@ -1,6 +1,7 @@
 from Character import Character
 from CharacterEvents import *
 from Configure_Gameplay import *
+from Utilities import load_image
 from Configure import SIZE
 import pygame
 import threading
@@ -46,15 +47,68 @@ class Player(pygame.sprite.Sprite):
         self.clock = clock
         self.pos_x, self.pos_y = pos
 
-        self.image = pygame.Surface((30, 30))
+        self.stay = load_image(['Gameplay/Character/idle1', 'adventurer-idle-00.png'])
+        self.image = self.stay
         self.rect = self.image.get_rect()
 
         self.rect.x, self.rect.y = self.pos_x * SIZE_CONST, self.pos_y * SIZE_CONST
         self.pos_x, self.pos_y = self.rect.x, self.rect.y
-        self.image.fill((255, 20, 0))
+        
 
 
         self.jump_enable = False
+        
+        self.fps = 0
+        self.walkRight = [load_image(['Gameplay/Character/run', 'adventurer-run-00.png']),
+                          load_image(['Gameplay/Character/run', 'adventurer-run-01.png']),
+                          load_image(['Gameplay/Character/run', 'adventurer-run-02.png']),
+                          load_image(['Gameplay/Character/run', 'adventurer-run-03.png']),
+                          load_image(['Gameplay/Character/run', 'adventurer-run-04.png']),
+                          load_image(['Gameplay/Character/run', 'adventurer-run-05.png'])]
+
+        self.walkLeft = [load_image(['Gameplay/Character/run', 'left-00.png']),
+                         load_image(['Gameplay/Character/run', 'left-01.png']),
+                         load_image(['Gameplay/Character/run', 'left-02.png']),
+                         load_image(['Gameplay/Character/run', 'left-03.png']),
+                         load_image(['Gameplay/Character/run', 'left-04.png']),
+                         load_image(['Gameplay/Character/run', 'left-05.png'])]
+
+        self.attackLeft = [load_image(['Gameplay/Character/default attack', 'left-00.png']),
+                           load_image(['Gameplay/Character/default attack', 'left-01.png']),
+                           load_image(['Gameplay/Character/default attack', 'left-02.png']),
+                           load_image(['Gameplay/Character/default attack', 'left-03.png']),
+                           load_image(['Gameplay/Character/default attack', 'left-04.png']),
+                           load_image(['Gameplay/Character/default attack', 'left-05.png'])]
+
+        self.attackRight = [load_image(['Gameplay/Character/default attack', 'adventurer-attack2-00.png']),
+                            load_image(['Gameplay/Character/default attack', 'adventurer-attack2-01.png']),
+                            load_image(['Gameplay/Character/default attack', 'adventurer-attack2-02.png']),
+                            load_image(['Gameplay/Character/default attack', 'adventurer-attack2-03.png']),
+                            load_image(['Gameplay/Character/default attack', 'adventurer-attack2-04.png']),
+                            load_image(['Gameplay/Character/default attack', 'adventurer-attack2-05.png'])]
+
+        self.airLeft = [load_image(['Gameplay/Character/air attack', 'left-00.png']),
+                        load_image(['Gameplay/Character/air attack', 'left-01.png']),
+                        load_image(['Gameplay/Character/air attack', 'left-02.png']),
+                        load_image(['Gameplay/Character/air attack', 'left-03.png'])]
+
+        self.airRight = [load_image(['Gameplay/Character/air attack', 'adventurer-air-attack1-00.png']),
+                         load_image(['Gameplay/Character/air attack', 'adventurer-air-attack1-01.png']),
+                         load_image(['Gameplay/Character/air attack', 'adventurer-air-attack1-02.png']),
+                         load_image(['Gameplay/Character/air attack', 'adventurer-air-attack1-03.png'])]
+
+        self.stay_images = [load_image(['Gameplay/Character/idle1', 'adventurer-idle-00.png']),
+                            load_image(['Gameplay/Character/idle1', 'adventurer-idle-01.png']),
+                            load_image(['Gameplay/Character/idle1', 'adventurer-idle-02.png'])]
+
+        self.is_right = False
+        self.is_left = False
+        self.attack = False
+        self.air_attack = False
+        self.stay_ = True
+        self.for_attack = 0
+        self.for_air_attack = 0
+        self.for_stay = 0
 
     def event_handler(self, event):
         time = self.clock[0] / 1000
@@ -65,3 +119,63 @@ class Player(pygame.sprite.Sprite):
             self.pos_x += SPEED_X * time
         self.rect.x = self.pos_x
         #print(self.pos_x)
+        elif event == MOVE_UP and self.jump_enable is False:
+            self.jump_enable = True
+            self.previous_y = self.rect.y
+            self.time_j = 0
+            self.clock.tick()
+        elif event == ATTACK:
+            self.attack = True
+            self.stay_ = False
+        elif event == DODGE_ATTACK:
+            self.air_attack = True
+            self.stay_ = False
+
+        self.render()
+        # print(self.is_right, self.is_left)
+
+    def render(self):
+        if self.fps + 1 >= 30:
+            self.fps = 0
+
+        if not self.is_left and not self.is_right:
+            self.image = self.stay
+
+        if self.is_left:
+            self.image = self.walkLeft[self.fps // 5]
+            self.fps += 1
+        elif self.is_right:
+            self.image = self.walkRight[self.fps // 5]
+            self.fps += 1
+        else:
+            self.image = self.stay
+
+        if self.attack:
+            if self.for_attack + 1 >= 30:
+                self.for_attack = 0
+            if self.is_left:
+                self.image = self.attackLeft[self.for_attack // 5]
+                self.for_attack += 1
+            elif self.is_right:
+                self.image = self.attackRight[self.for_attack // 5]
+                self.for_attack += 1
+            self.attack = False
+
+        if self.air_attack:
+            if self.for_air_attack + 1 >= 20:
+                self.for_air_attack = 0
+            if self.is_left:
+                self.image = self.airLeft[self.for_air_attack // 5]
+                self.for_air_attack += 1
+            elif self.is_right:
+                self.image = self.airRight[self.for_air_attack // 5]
+                self.for_air_attack += 1
+            self.air_attack = False
+
+        if self.for_stay + 1 >= 15:
+            self.for_stay = 0
+
+        if self.stay_:
+            self.image = self.stay_images[self.for_stay // 5]
+            self.for_stay += 1
+        # print(self.for_stay)
