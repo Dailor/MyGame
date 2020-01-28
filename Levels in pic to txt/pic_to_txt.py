282c48f8a68bad54e0de3f10910df7f5cf199681
khodec <py-seo@yandex.ru>
2020-01-29 05:28:07 +0600

    Добавил "переводчик" уровня из картинок в txt

---
+++ Levels in pic to txt/pic_to_txt.py
@@ -0,0 +1,32 @@
+from PIL import Image
+
+sprites = dict()
+sprites[(255, 255, 255)] = '.'
+sprites[(0, 0, 0)] = 'h'
+sprites[(34, 177, 76)] = 'g'
+sprites[(185, 122, 87)] = 'd'
+sprites[(127, 127, 127)] = 's'
+sprites[(237, 28, 36)] = "u"  # улитка
+sprites[(255, 242, 0)] = 'o'  # оса
+sprites[(143, 113, 48)] = 'p'  # Платформа
+sprites[(163, 73, 164)] = 's'  # Шипы
+sprites[(0, 162, 232)] = 'c'  # сундук
+sprites[(15, 240, 223)] = '@' # Player
+
+print("Введите название файла")
+name = input()
+image = Image.open(name)
+x, y = image.size
+pix = image.load()
+result = ""
+for j in range(y):
+    for i in range(x):
+        rgb = pix[i, j]
+        try:
+            result += sprites[rgb]
+        except Exception as e:
+            raise Exception(f"Нету {rgb}")
+    result += '\n'
+
+with open(name.rsplit('.', maxsplit=1)[0] + '.txt', 'w') as f:
+    f.write(result)
