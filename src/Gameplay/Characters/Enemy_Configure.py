3a2873ebb3c3f5c12bfe70c76c1f678124fd9428
Adil7777 <ismagambetov,adil2005@gmail.com>
2020-01-28 22:26:44 +0600

    Подключил спрайты к игре

---
+++ src/Gameplay/Characters/Enemy_Configure.py
@@ -0,0 +1,25 @@
+from Utilities import load_image
+
+Bee = [load_image(['Gameplay/Enemies/bee', 'bee-1.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-2.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-3.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-4.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-5.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-6.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-7.png']),
+       load_image(['Gameplay/Enemies/bee', 'bee-8.png'])]
+
+Piranha_Plant = [load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-1.png']),
+                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-2.png']),
+                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-3.png']),
+                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-4.png'])]
+
+Piranha_Plant_Attack = [load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-1.png']),
+                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-2.png']),
+                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-3.png']),
+                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-4.png'])]
+
+Slug = [load_image(['Gameplay/Enemies/slug', 'slug-1']),
+        load_image(['Gameplay/Enemies/slug', 'slug-2']),
+        load_image(['Gameplay/Enemies/slug', 'slug-3']),
+        load_image(['Gameplay/Enemies/slug', 'slug-4'])]
