431bf8b78df638888bf1412e591d3e4f3ec20420
khodec <py-seo@yandex.ru>
2020-02-01 22:43:36 +0600

    еще докинул

--- src/Gameplay/Enemies/Enemy_Configure.py
+++ src/Gameplay/Enemies/Enemy_Configure.py
@@ -1,29 +1,32 @@
-from Utilities import load_image
+from Utilities import load_image_v2
 import pygame
 
 pygame.init()
 pygame.display.set_mode()
 
-Bee_stay = [load_image(['Gameplay/Enemies/bee', 'bee-1.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-2.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-3.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-4.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-5.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-6.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-7.png']),
-            load_image(['Gameplay/Enemies/bee', 'bee-8.png'])]
+SIZE_ENEMY = 70, 70
 
-Piranha_Plant = [load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-1.png']),
-                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-2.png']),
-                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-3.png']),
-                 load_image(['Gameplay/Enemies/piranha-plant', 'piranha-plant-4.png'])]
+Bee_stay = [load_image_v2(['Gameplay/Enemies/bee', 'bee-1.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-2.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-3.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-4.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-5.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-6.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-7.png'], SIZE_ENEMY),
+            load_image_v2(['Gameplay/Enemies/bee', 'bee-8.png'], SIZE_ENEMY)]
 
-Piranha_Plant_Attack = [load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-1.png']),
-                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-2.png']),
-                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-3.png']),
-                        load_image(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-4.png'])]
+Piranha_Plant = [load_image_v2(['Gameplay/Enemies/piranha-plant', 'piranha-plant-1.png'], SIZE_ENEMY),
+                 load_image_v2(['Gameplay/Enemies/piranha-plant', 'piranha-plant-2.png'], SIZE_ENEMY),
+                 load_image_v2(['Gameplay/Enemies/piranha-plant', 'piranha-plant-3.png'], SIZE_ENEMY),
+                 load_image_v2(['Gameplay/Enemies/piranha-plant', 'piranha-plant-4.png'], SIZE_ENEMY)]
 
-Slug_stay = [load_image(['Gameplay/Enemies/slug', 'slug-1.png']),
-        load_image(['Gameplay/Enemies/slug', 'slug-2.png']),
-        load_image(['Gameplay/Enemies/slug', 'slug-3.png']),
-        load_image(['Gameplay/Enemies/slug', 'slug-4.png'])]
+Piranha_Plant_Attack = [
+    load_image_v2(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-1.png'], SIZE_ENEMY),
+    load_image_v2(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-2.png'], SIZE_ENEMY),
+    load_image_v2(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-3.png'], SIZE_ENEMY),
+    load_image_v2(['Gameplay/Enemies/piranha-plant-attack', 'piranha-plant-attack-4.png'], SIZE_ENEMY)]
+
+Slug_stay = [load_image_v2(['Gameplay/Enemies/slug', 'slug-1.png'], SIZE_ENEMY),
+             load_image_v2(['Gameplay/Enemies/slug', 'slug-2.png'], SIZE_ENEMY),
+             load_image_v2(['Gameplay/Enemies/slug', 'slug-3.png'], SIZE_ENEMY),
+             load_image_v2(['Gameplay/Enemies/slug', 'slug-4.png'], SIZE_ENEMY)]
--- src/Menu/Utilities.py
+++ src/Menu/Utilities.py
@@ -70,5 +70,4 @@ def load_image(path, colorkey=None):
 def load_image_v2(path, size=None):
     image = load_image(path, None)
     image = pygame.transform.scale(image, size)
-    print(size)
     return image
